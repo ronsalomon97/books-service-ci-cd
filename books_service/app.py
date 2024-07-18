307cc74cd7037a2ddbe4a38a1f431a6ed99bf263
Ron Salomon <ronsalomon97@gmail.com>
2024-07-18 16:02:31 +0300

    Added health check to app and changed it to 5001 instead of 8000

--- books_service/app.py
+++ books_service/app.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask
+from flask import Flask, jsonify
 from pymongo import MongoClient
 from routes.book_routes import book_routes
 from routes.rating_routes import rating_routes
@@ -14,5 +14,10 @@ app.config['db'] = client['booksdb']
 app.register_blueprint(book_routes)
 app.register_blueprint(rating_routes)
 
+# Health check endpoint
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "healthy"}), 200
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
